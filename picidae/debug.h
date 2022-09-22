f76dcac1247f65fb76c382e33df6ac02717621d2
CanftIn <wwc7033@gmail.com>
2022-09-22 11:08:12 +0800

    update

---
+++ picidae/debug.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace picidae {
+
+constexpr bool enable_assert = true;
+
+//#define PICI_ASSERT_INFO(x, msg) pici_assert(context, (int)(x), msg)
+//#define PICI_ASSERT(x) PICI_ASSERT_INFO(x, #x)
+
+
+
+} // namespace picidae
