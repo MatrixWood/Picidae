179e949cef842e8635e89ee03e46cda20af815a8
CanftIn <wwc7033@gmail.com>
2022-11-15 07:16:36 +0000

    update lightweighttest

--- picidae/lightweighttest/lightweighttest.hpp
+++ picidae/lightweighttest/lightweighttest.hpp
@@ -500,6 +500,103 @@ inline int report_errors() {
   { EXPR; }
 #endif
 
+template <class, int = 0>
+struct test_print {};
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<T, 2>) {
+  return o << std::string(typeid(T));
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<T, 1>) {
+  return o << test_print<T, 2>();
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<const T, 1>) {
+  return o << test_print<T, 2>() << " const";
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<volatile T, 1>) {
+  return o << test_print<T, 2>() << " volatile";
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o,
+                                test_print<const volatile T, 1>) {
+  return o << test_print<T, 2>() << " const volatile";
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<T>) {
+  return o << test_print<T, 1>();
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<T&>) {
+  return o << test_print<T, 1>() << " &";
+}
+
+template <class T>
+inline std::ostream& operator<<(std::ostream& o, test_print<T&&>) {
+  return o << test_print<T, 1>() << " &&";
+}
+
+template <class T>
+inline void test_trait_impl(char const* trait, void (*)(T), bool expected,
+                            char const* file, int line, char const* function) {
+  if (T::value == expected) {
+    test_results();
+  } else {
+    std::cerr << PICIDAE_LIGHTWEIGHTTEST_TERM_GREEN
+              << PICIDAE_LIGHTWEIGHTTEST_NAME << file << "(" << line
+              << "): predicate '" << trait << "' [" << std::string(typeid(T))
+              << "]"
+              << " test failed in function '" << function
+              << "' (should have been " << (expected ? "true" : "false") << ")"
+              << PICIDAE_LIGHTWEIGHTTEST_TERM_RED << std::endl;
+
+    ++test_results().errors();
+  }
+}
+
+template <class T>
+inline bool test_trait_same_impl_(T) {
+  return T::value;
+}
+
+template <class T1, class T2>
+inline void test_trait_same_impl(char const* types, std::is_same<T1, T2> same,
+                                 char const* file, int line,
+                                 char const* function) {
+  if (test_trait_same_impl_(same)) {
+    test_results();
+  } else {
+    std::cerr << PICIDAE_LIGHTWEIGHTTEST_TERM_GREEN
+              << PICIDAE_LIGHTWEIGHTTEST_NAME << file << "(" << line
+              << "): test 'is_same<" << types << ">'"
+              << " failed in function '" << function << "' ('"
+              << test_print<T1>() << "' != '" << test_print<T2>() << "')"
+              << PICIDAE_LIGHTWEIGHTTEST_TERM_RED << std::endl;
+
+    ++test_results().errors();
+  }
+}
+
+#define BOOST_TEST_TRAIT_TRUE(type)                                         \
+  (::boost::detail::test_trait_impl(#type, (void(*) type)0, true, __FILE__, \
+                                    __LINE__, BOOST_CURRENT_FUNCTION))
+#define BOOST_TEST_TRAIT_FALSE(type)                                         \
+  (::boost::detail::test_trait_impl(#type, (void(*) type)0, false, __FILE__, \
+                                    __LINE__, BOOST_CURRENT_FUNCTION))
+
+#define BOOST_TEST_TRAIT_SAME(...)                                             \
+  (::boost::detail::test_trait_same_impl(                                      \
+      #__VA_ARGS__, ::boost::core::is_same<__VA_ARGS__>(), __FILE__, __LINE__, \
+      BOOST_CURRENT_FUNCTION))
+
 PICIDAE_NAMESPACE_END(lightweighttest)
 
 PICIDAE_NAMESPACE_END(PICIDAE_NAMESPACE)
