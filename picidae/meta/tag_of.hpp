fe3b0dd664ceac32577d7a45c1f0a81babfdce80
CanftIn <wwc7033@gmail.com>
2022-11-16 07:27:56 +0000

    add tag of and organize test fold

---
+++ picidae/meta/tag_of.hpp
@@ -0,0 +1,54 @@
+#ifndef PICIDAE_META_TAG_OF_HPP__
+#define PICIDAE_META_TAG_OF_HPP__
+
+#include "picidae/meta/macro.hpp"
+#include "picidae/meta/when.hpp"
+
+PICIDAE_NAMESPACE_BEGIN(PICIDAE_NAMESPACE)
+
+PICIDAE_NAMESPACE_BEGIN(meta)
+
+template <typename T, typename = void>
+struct tag_of;
+
+template <typename T>
+using tag_of_t = typename tag_of<T>::type;
+
+template <typename T, typename>
+struct tag_of : tag_of<T, when<true>> {};
+
+PICIDAE_NAMESPACE_BEGIN(core_detail)
+
+template <typename...>
+struct is_valid {
+  static constexpr bool value = true;
+};
+
+PICIDAE_NAMESPACE_END(core_detail)
+
+template <typename T, bool Condition>
+struct tag_of<T, when<Condition>> {
+  using type = T;
+};
+
+template <typename T>
+struct tag_of<T, when<core_detail::is_valid<typename T::hana_tag>::value>> {
+  using type = typename T::hana_tag;
+};
+
+template <typename T>
+struct tag_of<T const> : tag_of<T> {};
+template <typename T>
+struct tag_of<T volatile> : tag_of<T> {};
+template <typename T>
+struct tag_of<T const volatile> : tag_of<T> {};
+template <typename T>
+struct tag_of<T&> : tag_of<T> {};
+template <typename T>
+struct tag_of<T&&> : tag_of<T> {};
+
+PICIDAE_NAMESPACE_END(meta)
+
+PICIDAE_NAMESPACE_END(PICIDAE_NAMESPACE)
+
+#endif  // PICIDAE_META_TAG_OF_HPP__
