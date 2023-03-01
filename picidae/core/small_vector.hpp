535bb1365f8a3cfbc85371765b6873ffd3c760f4
CanftIn <wwc7033@gmail.com>
2023-03-01 11:45:11 +0000

    update small_vector(build error)

--- picidae/core/small_vector.hpp
+++ picidae/core/small_vector.hpp
@@ -4,12 +4,17 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
 #include <initializer_list>
 #include <iterator>
+#include <limits>
 #include <memory>
+#include <type_traits>
 
+#include "picidae/core/arrayref.hpp"
+#include "picidae/core/iterator_range.hpp"
 #include "picidae/meta/macro.hpp"
 
 #if defined(__GNUC__)
@@ -39,36 +44,74 @@ inline uint64_t next_capacity(uint64_t A) {
 PICIDAE_NAMESPACE_END(detail)
 
 template <typename T>
+class ArrayRef;
+
+template <typename IteratorT>
+class iterator_range;
+
+template <typename T>
 struct IsPod : std::integral_constant<bool, std::is_standard_layout<T>::value &&
                                                 std::is_trivial<T>::value> {};
 
+template <class Size_T>
 class SmallVectorBase {
  protected:
-  void* _beg;
-  void* _end;
-  void* _capacity;
+  void *BeginX;
+  Size_T Size = 0, Capacity;
 
- protected:
-  SmallVectorBase(void* first_elem, size_t size)
-      : _beg(first_elem),
-        _end(first_elem),
-        _capacity((char*)first_elem + size) {}
+  static constexpr size_t SizeTypeMax() {
+    return std::numeric_limits<Size_T>::max();
+  }
+
+  SmallVectorBase() = delete;
+  SmallVectorBase(void *FirstEl, size_t TotalCapacity)
+      : BeginX(FirstEl), Capacity(TotalCapacity) {}
+
+  void* mallocForGrow(void *FirstEl, size_t MinSize, size_t TSize,
+                      size_t& NewCapacity);
+  
+  void grow_pod(void *FirstEl, size_t MinSize, size_t TSize);
+
+  void *replaceAllocation(void *NewElts, size_t TSize, size_t NewCapacity,
+                          size_t VSize = 0);
 
  public:
-  size_t size_in_bytes() const {
-    return size_t((char*)_end - (char*)_beg);
-  }
+  size_t size() const { return Size; }
+  size_t capacity() const { return Capacity; }
+  [[nodiscard]] bool empty() const { return !Size; }
 
-  size_t capacity_in_bytes() const {
-    return size_t((char*)_capacity - (char*)_beg);
-  }
-
-  bool empty() const {
-    return _beg == _end;
+ protected:
+  void set_size(size_t N) {
+    assert(N <= capacity());
+    Size = N;
   }
 };
 
+template <class T>
+using SmallVectorSizeType =
+    std::conditional_t<sizeof(T) < 4 && sizeof(void *) >= 8, uint64_t,
+                       uint32_t>;
 
+// TODO
+template <class T, typename = void>
+struct SmallVectorAlignmentAndSize {
+  alignas(SmallVectorBase<SmallVectorSizeType<T>>)
+  char Base[sizeof(SmallVectorBase<SmallVectorSizeType<T>>)];
+  
+};
+
+template <typename T, typename = void>
+class SmallVectorTemplateCommon
+    : public SmallVectorBase<SmallVectorSizeType<T>> {
+  using Base = SmallVectorBase<SmallVectorSizeType<T>>;
+
+ protected:
+  void* getFirstEl() const {
+    return const_cast<void *>(reinterpret_cast<const void*>(
+
+    ))
+  }
+};
 
 PICIDAE_NAMESPACE_END(core)
 
